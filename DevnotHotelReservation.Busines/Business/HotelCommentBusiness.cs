d2956d42c22864c05f158f28cd2f6b7bf446cbb4
Emrah <inamemrah@gmail.com>
2019-07-14 19:39:00 +0300

    Kullanıcının otel ile ilgili yorum yapması yapıldı Database .bak ve .sql olarak proje dosyasına eklendi

---
+++ DevnotHotelReservation.Busines/Business/HotelCommentBusiness.cs
@@ -0,0 +1,35 @@
+﻿using DevnotHotelReservation.DAL.Repository;
+using DevnotHotelReservation.Entity.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevnotHotelReservation.Busines.Business
+{
+    public class HotelCommentBusiness
+    {
+        private HotelDBContext hotelDBContext;
+
+        public HotelCommentBusiness()
+        {
+            HotelDBContext hotelDBContext = new HotelDBContext();
+            hotelDBContext = this.hotelDBContext;
+        }
+
+        public void AddComment(string Comment, int Score, int Id, int UserId)
+        {
+            HotelCommentRepository hotelCommentRepository = new HotelCommentRepository(hotelDBContext);
+
+            HotelComment hotelComment = new HotelComment
+            {
+                Comment = Comment,
+                Score = Score,
+                HotelId = Id,
+                UserId = UserId
+
+            };
+
+            hotelCommentRepository.Insert(hotelComment);
+        }
+    }
+}
---
+++ DevnotHotelReservation.DAL/Repository/HotelCommentRepository.cs
@@ -0,0 +1,17 @@
+﻿using DevnotHotelReservation.Entity.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevnotHotelReservation.DAL.Repository
+{
+    public class HotelCommentRepository : Repository<HotelComment>
+    {
+        private HotelDBContext hotelDBContext;
+
+        public HotelCommentRepository(HotelDBContext hotelDBContext) : base(hotelDBContext)
+        {
+            hotelDBContext = this.hotelDBContext;
+        }
+    }
+}
